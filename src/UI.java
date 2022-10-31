11757f2123083b7f15f0030bf1c7cbad7ef2ac9e
FilipEdlund01 <edlundfilip84@gmail.com>
2022-10-31 17:18:10 +0100

    drawing polygon

--- src/UI.java
+++ src/UI.java
@@ -1,10 +1,10 @@
 import java.awt.*;
 import javax.swing.*;
+import java.lang.Math;
 
 
 public class UI extends JPanel {
-    //  int[] xPoints = {200,300,350,300,200,150};
-    // int[] yPoints = {350,350,250,150,150,250};
+
 
     public Polygon poly;
     public Polygon poly2;
@@ -16,15 +16,78 @@ public class UI extends JPanel {
     public Polygon Poly3;
     public Polygon Poly4;
     public Polygon Poly5;
+    public int a = 50;
+
+   /* Let the length of the sides of the regular hexagon be  a  units.
+
+⇒  The coordinates of vertex  A  and vertex  D  are  (a,0)  and  (−a,0)  respectively.
+
+    Each interior angle of a regular hexagon is  120o.
+
+⇒  The triangle formed by each side and the lines joining the end points of the side to the circumcentre are equilateral.
+
+⇒  The circumradius of the hexagon is  a  units.
+
+            ⇒  The coordinates of vertex  B  are  (acos60o,asin60o)=(a2,3√a2).
+
+            ⇒  The coordinates of vertex  C  are  (acos120o,asin120o)=(−a2,3√a2).
+
+            ⇒  The coordinates of vertex  E  are  (acos240o,asin240o)=(−a2,−3√a2).
+
+            ⇒  The coordinates of vertex  F  are  (acos300o,asin300o)=(a2,−3√a2).*/
+
+    // https://www.quora.com/How-can-you-find-the-coordinates-in-a-hexagon
+
+
+    public double Ex = 500 - (a / 2);
+    public double Ey = 500 - ((Math.sqrt(3) * a) / 2);
+
+    public double Fx = 500 + (a / 2);
+    public double Fy = 500 - ((Math.sqrt(3) * a) / 2);
+
+    public double Ax = 500 + a;
+    public double Ay = 500;
+
+    public double Dx = 500 - a;
+    public double Dy = 500;
+
+    public double Bx = 500 + (a / 2);
+    public double By = 500 + ((Math.sqrt(3) * a) / 2);
+
+    public double Cx = 500 - (a / 2);
+    public double Cy = 500 + ((Math.sqrt(3) * a) / 2);
+
+
+    int EX = (int) Ex;
+    int EY = (int) Ey;
+
+    int FX = (int) Fx;
+    int FY = (int) Fy;
+
+    int AX = (int) Ax;
+    int AY = (int) Ay;
+
+    int DX = (int) Dx;
+    int DY = (int) Dy;
+
+    int BX = (int) Bx;
+    int BY = (int) By;
+
+    int CX = (int) Cx;
+    int CY = (int) Cy;
 
 
     public boolean drawed = false;
-    int[] xPoints = {475, 525, 550, 525, 475, 450};
-    int[] yPoints = {550, 550, 500, 450, 450, 500};
+    int[] xPoints = {EX, FX, AX, BX, CX, DX};
+    int[] yPoints = {EY, FY, AY, BY, CY, DY};
+
+     /* int[] xPoints = {900,1000,1050,1000,900,850};// vytvori sipku -muze se hodit
+        int[] yPoints = {500,500,400,300,300,400};
+*/
 
 
     public UI() {
-        //movePolygons();
+
         initComponents();
 
 
@@ -49,15 +112,15 @@ public class UI extends JPanel {
     public void movePolygons() {
 
 
-        poly2.translate(0, -105);
+        poly2.translate(0, -90);
 
-        poly3.translate(0, 105);
+        /*poly3.translate(0, 105);
 
 
         poly4.translate(0, 50);
 
 
-        poly5.translate(0, 100);
+        poly5.translate(0, 100);*/
 
 
         Poly2 = poly2;
@@ -74,12 +137,6 @@ public class UI extends JPanel {
     public void paintComponent(Graphics g) {
 
 
-
-      /*  public void Polygon (int[] xPoints, int[] yPoints, int numPoints){
-            g.fillPolygon(xPoints, yPoints, xPoints.length);
-        }*/
-
-
         super.paintComponent(g);
         this.setBackground(Color.white);
 
@@ -88,9 +145,6 @@ public class UI extends JPanel {
        /* int[] xPoints = {900,1000,950,1000,900,850};// vytvori sipku -muze se hodit
         int[] yPoints = {500,500,400,300,300,400};*/
 
-      /*  int[] xPoints = {900,1000,1050,1000,900,850};
-        int[] yPoints = {600,600,500,400,400,500};
-        g.fillPolygon(xPoints, yPoints, xPoints.length);*/
 
         // first polygon in center
 
@@ -99,7 +153,7 @@ public class UI extends JPanel {
             g.drawPolygon(poly);
 
 
-        /* g.drawPolygon(Poly2);
+            g.drawPolygon(Poly2);
 
 
             g.drawPolygon(Poly3);
@@ -107,16 +161,7 @@ public class UI extends JPanel {
             g.drawPolygon(Poly4);
 
 
-            g.drawPolygon(Poly5);*/
-
-
-
-
-
-
-
-       /* Image chessPiecesImage;
-        chessPiecesImage=new ImageIcon("ChessPieces.png").getImage();*/
+            g.drawPolygon(Poly5);
 
 
         }
